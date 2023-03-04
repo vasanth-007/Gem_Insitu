f0bbc659de0e160f6077992148fea8b19c7c7e48
vasantharaman <vasantharaman@iamneo.ai>
2023-03-04 10:36:27 +0530

    added animation effect

--- assets/js/leadmark.js
+++ assets/js/leadmark.js
@@ -1,20 +1,7 @@
-/*!
-=========================================================
-* LeadMark Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 
 // smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+$(document).ready(function () {
+    $(".navbar .nav-link").on('click', function (event) {
 
         if (this.hash !== "") {
 
@@ -24,15 +11,15 @@ $(document).ready(function(){
 
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 700, function(){
+            }, 700, function () {
                 window.location.hash = hash;
             });
-        } 
+        }
     });
 });
 
 // protfolio filters
-$(window).on("load", function() {
+$(window).on("load", function () {
     var t = $(".portfolio-container");
     t.isotope({
         filter: ".new",
@@ -41,7 +28,7 @@ $(window).on("load", function() {
             easing: "linear",
             queue: !1
         }
-    }), $(".filters a").click(function() {
+    }), $(".filters a").click(function () {
         $(".filters .active").removeClass("active"), $(this).addClass("active");
         var i = $(this).attr("data-filter");
         return t.isotope({
